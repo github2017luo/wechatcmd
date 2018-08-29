d7c26b0bd8e7c36464d386f6e83a68b6097d5254
王彦民 <yanmin.wang@ele.me>
2018-08-29 20:02:29 +0800

    fix: import

--- main.go
+++ main.go
@@ -9,8 +9,8 @@ import (
 	"path"
 
 	ct "github.com/daviddengcn/go-colortext"
-	"github.com/liushuchun/wechatcmd/ui"
-	chat "github.com/liushuchun/wechatcmd/wechat"
+	"./ui"
+	chat "./wechat"
 )
 
 const (
